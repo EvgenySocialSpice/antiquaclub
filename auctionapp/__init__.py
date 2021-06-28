1323efdeaa2da0368a189868724383ba715d7cf7
Evgeny Surogin <sacccp@gmail.com>
2021-06-28 23:45:09 +0300

    Верстка главной страницы, макет контактов и о нас, папки статик и имиджи

--- auctionapp/__init__.py
+++ auctionapp/__init__.py
@@ -1,11 +1,22 @@
-from flask import Flask 
+from flask import Flask, flash, render_template, redirect, url_for
+
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_pyfile("config.py")
 
     @app.route('/')
-    def hello_world_page():
-        return "Hello world!"
-    
+    def index():
+        title = 'АнтиквА Аукцион онлайн'
+        return render_template('site/index.html', page_title=title)
+
+    @app.route('/about')
+    def about():
+        title = 'О нас'
+        return render_template('site/about.html', page_title=title)
+
+    @app.route('/contacts')
+    def contacts():
+        title = 'Контакты'
+        return render_template('site/contacts.html', page_title=title)
+
     return app
