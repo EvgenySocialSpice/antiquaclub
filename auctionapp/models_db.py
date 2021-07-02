cedf9c8c4926e08e548c93ac92f9fab8b665eb8e
Vladimir Shatskiy <vovaysach@gmail.com>
2021-07-02 20:10:21 +0300

    add tags + add some atributes to other database

--- auctionapp/models_db.py
+++ auctionapp/models_db.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, DateTime, Date, Time, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, Date, Time, ForeignKey, Boolean
+from sqlalchemy_utils import EmailType
 from sqlalchemy.orm import relationship
 from auctionapp.db import Base, engine
 
@@ -10,13 +11,16 @@ class User(Base):
     name = Column(String(), nullable=False)
     last_name = Column(String(), nullable=False)
     nickname = Column(String(), unique=True, nullable=False, index=True)
-    email = Column(String(120), unique=True, nullable=False, index=True)
+    email = Column(EmailType(), unique=True, nullable=False, index=True)
     phone = Column(Integer(), unique=True, nullable=False)
     birth_date = Column(Date(), nullable=False)
-    reg_datetime = Column(DateTime())
-    password = Column(String())
-    role_user = Column(String())
+    reg_datetime = Column(DateTime(), nullable=False)
+    password = Column(String(), nullable=False)
+    role_user = Column(String(15))
     items = relationship("Item", lazy="joined")
+    bets = relationship("Bet", lazy="joined")
+    phone_confirmed = Column(Boolean())
+    email_confirmed = Column(Boolean())
 
     def __repr__(self):
         return f"User {self.id}, {self.nickname}, {self.email}"
@@ -26,7 +30,7 @@ class Category(Base):
     __tablename__ = "categories"
 
     id = Column(Integer(), primary_key=True)
-    name = Column(String(), nullable=False, index=True)
+    name = Column(String(), nullable=False, index=True, unique=True)
     items = relationship("Item", lazy="joined")
 
     def __repr__(self):
@@ -36,17 +40,17 @@ class Category(Base):
 class Item(Base):
     __tablename__ = "items"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer(), primary_key=True)
     name = Column(String(), nullable=False, index=True)
     category_id = Column(Integer(), ForeignKey(Category.id), index=True, nullable=False)
-    # tags = Column(ARRAY(String(10)))
     year = Column(String())
     description = Column(String())
-    reg_time = Column(DateTime())
-    nom_price = Column(Integer())
+    reg_time = Column(DateTime(), nullable=False)
+    nom_price = Column(Integer(), nullable=False)
     step_price = Column(Integer())
     last_price = Column(Integer())
     start_auction = Column(DateTime())
+    success_end_time = Column(DateTime())
     max_time_duration = Column(Time())
     seller_user_id = Column(Integer(), ForeignKey(User.id), index=True, nullable=False)
     buyer_user_id = Column(Integer(), ForeignKey(User.id), index=True)
@@ -54,6 +58,7 @@ class Item(Base):
     category = relationship("Category", lazy="joined")
     users = relationship("User", lazy="joined")
     bets = relationship("Bet", lazy="joined")
+    tags = relationship("ItemTag", lazy="joined")
 
     def __repr__(self):
         return f"Item {self.id}, {self.name}"
@@ -63,10 +68,11 @@ class Bet(Base):
     __tablename__ = "bets"
 
     id = Column(Integer, primary_key=True)  
-    user_id = Column(Integer(), ForeignKey(User.id), index=True)
-    item_id = Column(Integer(), ForeignKey(Item.id), index=True)
-    trans_time = Column(DateTime())
-    current_price = Column(Integer())
+    user_id = Column(Integer(), ForeignKey(User.id), index=True, nullable=False)
+    item_id = Column(Integer(), ForeignKey(Item.id), index=True, nullable=False)
+    trans_time = Column(DateTime(), nullable=False, index=True)
+    current_price = Column(Integer(), index=True, nullable=False)
+    bet_type = Column(String(20), index=True, nullable=False)
     user = relationship("User", lazy="joined")
     item = relationship("Item", lazy="joined")
 
@@ -74,17 +80,29 @@ class Bet(Base):
         return f"bet {self.id} by {self.user_id} at {self.trans_time}"
 
 
-# class Tag(Base):
-#     __tablename__ = "tags"
+class Tag(Base):
+    __tablename__ = "tags"
 
-#     id = Column(Integer(), primary_key=True)
-#     name = Column(String(50))
-#     tags_items_ids = Column(ARRAY(ForeignKey(Items.id)))
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(), nullable=False, index=True, unique=True)
+    items = relationship("ItemTag", lazy="joined")
 
-#     def __repr__(self):
-#         return f"Item {self.id}, {self.name}"
+    def __repr__(self):
+        return f"Tag {self.id}, {self.name}"
+
+
+class ItemTag(Base):
+    __tablename__ = "items_tags"
+
+    id = Column(Integer(), primary_key=True)
+    item_id = Column(Integer(), ForeignKey(Item.id), index=True, nullable=False)
+    tag_id = Column(Integer(), ForeignKey(Tag.id), index=True, nullable=False)
+    item = relationship("ItemTag", lazy="joined")
+    tag = relationship("Tag", lazy="joined")
+
+    def __repr__(self):
+        return f"Товар {self.item_id} тэг {self.tag_id}"
 
 
 def create_models():
     Base.metadata.create_all(bind=engine)
-
