dc34176e6819fdaf493709ce2724940d8c7a731e
Evgeny Surogin <sacccp@gmail.com>
2021-07-14 13:49:28 +0300

    Исправил инит и db.py

--- auctionapp/__init__.py
+++ auctionapp/__init__.py
@@ -5,7 +5,7 @@ from auctionapp.models_db import Base
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile("config.py")
-    # Base.init_app(app)
+    #Base.init_app(app)
 
     @app.route('/')
     def index():
