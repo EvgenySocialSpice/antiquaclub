ea8b87c7548f8921d716b592872ca6a2b43609c2
Vladimir Shatskiy <vovaysach@gmail.com>
2021-07-30 22:41:11 +0300

    working with blueprints

---
+++ auctionapp/user/views.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, render_template
+from site_func import get_categories_cache
+
+blueprint = Blueprint("user", __name__, url_prefix="/users")
+
+
+@blueprint.route('/login')
+def login():
+    title = 'Авторизация'
+    categories = get_categories_cache()
+    return render_template('user/login.html', page_title=title, categories=categories)
+
+
