15ce5c9f07bb29594c43fcff881a5f51a2c6b613
Evgeny Surogin <sacccp@gmail.com>
2021-08-02 23:58:01 +0300

    Создал формы логин и регистрации и соответствующие файлы, где-то ошибка

--- auctionapp/__init__.py
+++ auctionapp/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_login import LoginManager, current_user, login_required
+
+from auctionapp.models_db import User
 from auctionapp.user.views import blueprint as user_blueprint
 from auctionapp.site.views import blueprint as site_blueprint
 from auctionapp.item.views import blueprint as item_blueprint
@@ -9,8 +13,18 @@ def create_app():
     app = Flask(__name__)
     app.config.from_pyfile("config.py")
     db.init_app(app)
+    migrate = Migrate(app, db)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'user.login'
+
     app.register_blueprint(user_blueprint)
     app.register_blueprint(item_blueprint)
     app.register_blueprint(site_blueprint)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
 
     return app
--- auctionapp/user/views.py
+++ auctionapp/user/views.py
@@ -1,4 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, flash
+from flask_login import current_user, login_user, logout_user
+from auctionapp.user.forms import LoginForm, RegistrationForm
+from auctionapp.db import db
+from auctionapp.user.models import User
+from auctionapp.utils import get_redirect_target
 from auctionapp.site_func import get_categories_cache
 
 blueprint = Blueprint("user", __name__, url_prefix="/users")
@@ -7,7 +12,57 @@ blueprint = Blueprint("user", __name__, url_prefix="/users")
 @blueprint.route('/login')
 def login():
     title = 'Авторизация'
+    login_form = LoginForm()
     categories = get_categories_cache()
-    return render_template('user/login.html', page_title=title, categories=categories)
+    return render_template('user/login.html', page_title=title, form=login_form, categories=categories)
 
 
+@blueprint.route('/process-login', methods=['POST'])
+def process_login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            flash('Вы успешно вошли на сайт')
+            return redirect(get_redirect_target())
+
+    flash('Неправильные имя пользователя или пароль')
+    return redirect(url_for('user.login'))
+
+
+@blueprint.route('/logout')
+def logout():
+    logout_user()
+    flash('Вы успешно разлогинились')
+    return redirect(url_for('site.index'))
+
+
+@blueprint.route('/register')
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('site.index'))
+    title = "Регистрация"
+    form = RegistrationForm()
+    return render_template('user/registration.html', page_title=title, form=form)
+
+
+@blueprint.route('/process-reg', methods=['POST'])
+def process_reg():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        news_user = User(nickname=form.nick_name.data, email=form.email.data, role='user')
+        news_user.set_password(form.password.data)
+        db.session.add(news_user)
+        db.session.commit()
+        flash('Вы успешно зарегистрировались!')
+        return redirect(url_for('user.login'))
+    else:
+        for field, errors in form.errors.items():
+            for error in errors:
+                flash('Ошибка в поле {}: {}'.format(
+                    getattr(form, field).label.text,
+                    error
+                ))
+        return redirect(url_for('user.register'))
